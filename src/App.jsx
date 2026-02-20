8ccd0e9f0ce40b486244eb585bdc55f2b514bc37
Mahmoud Saaed Romeh <romeheng1@gmail.com>
2026-02-20 16:39:43 +0200

    نسال الله التوفيق و السداد

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+       <div className="bg-gradient-to-r from-purple-500 to-blue-500 text-white text-4xl p-10 rounded-2xl shadow-2xl">
+  جاهزين نبني بقا 😎
+</div>
+    </>
+  )
+}
+
+export default App
